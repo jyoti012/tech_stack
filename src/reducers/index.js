c90ac79dcff950f208a67926c45652721b24f301
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-19 11:47:50 +0530

    creating selectLibrary reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './libraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 export default combineReducers({
-  libraries: LibraryReducer // libraries - name of the reducer, returns array
+  libraries: LibraryReducer, // libraries - name of the reducer, returns array
+  selectedLibraryId: SelectionReducer
 });
