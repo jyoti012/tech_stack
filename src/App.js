e3a322a9d7a0d6747e32b7c512686e68e531cd16
Jyoti Suvarna <jyoti@ajackus.com>
2018-10-29 13:16:20 +0530

    adding button to top right in header

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ class App extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
       title: navigation.getParam('otherParam', 'Home'),
-      headerStyle: {
-        backgroundColor: '#f4511e',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
+      headerRight: (
+        <TouchableOpacity
+          onPress={() => alert('Contact you soon')}
+          style={{ height: 50, width: 100, textAlign: 'center', borderRadius: 5  }} >
+          <Text style={{ paddingTop: 15, paddingBottom: 10, alignSelf: 'center', color: '#fff', }}>Help</Text>
+        </TouchableOpacity>
+      ),
     };
   };
 
@@ -22,12 +22,12 @@ class App extends React.Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
         <TouchableOpacity onPress={() => { this.props.navigation.push('Details', 
-          {
-            itemId: Math.floor(Math.random() * 100),
-            title: 'Details',
-            otherParam: 'Lorem Ipsum',
-          }) 
-        }} >
+            {
+              itemId: Math.floor(Math.random() * 100),
+              title: 'Details',
+              otherParam: 'Lorem Ipsum',
+            }) 
+          }} >
             <Text style={{ 
               borderWidth: 1, 
               height: 50, 
@@ -46,13 +46,6 @@ class DetailsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
       title: navigation.getParam('title', 'Details page'),
-      headerStyle: {
-        backgroundColor: '#f4511e',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
     };
   };
   render() {
@@ -91,4 +84,14 @@ export default createStackNavigator({
   },
 }, {
     initialRouteName: 'Home',
+    /* The header config from HomeScreen is now here */
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
 });
